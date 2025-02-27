bff135de4780ef7f926ec9841a6bf4b91e7bfb66
Patricio Yu <yupatricio0@gmail.com>
2025-02-27 20:13:18 -0300

    cambie visibilidad de las instancias de los repositorios

--- src/main/java/com/bazar/service/ClienteService.java
+++ src/main/java/com/bazar/service/ClienteService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 public class ClienteService implements IClienteService {
     @Autowired
-    IClienteRepository clienteRepository;
+    private IClienteRepository clienteRepository;
 
 
     @Override
--- src/main/java/com/bazar/service/VentaService.java
+++ src/main/java/com/bazar/service/VentaService.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Service
 public class VentaService implements IVentaService{
     @Autowired
-    IVentaRepository ventaRepository;
+    private IVentaRepository ventaRepository;
 
     @Override
     public void postVenta(Venta venta) {
